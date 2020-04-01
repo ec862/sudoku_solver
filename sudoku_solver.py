88fcf5ce8d788f2f969167cb9985b774ca370f50
ec862 <60794561+ec862@users.noreply.github.com>
2020-04-01 17:30:54 +0100

    Add files via upload

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+
 """
 Created on Wed Mar 11 17:53:54 2020
 
@@ -10,10 +10,6 @@ import numpy as np
 # Load sudokus
 sudokus = np.load("data/sudokus.npy")
 print("Shape of one sudoku array:", sudokus[0].shape, ". Type of array values:", sudokus.dtype)
-
-# Load solutions
-solutions = np.load("data/sudoku_solutions.npy")
-print("Shape of one sudoku solution array:", solutions[0].shape, ". Type of array values:", solutions.dtype, "\n")
 
 def move_possible(x,y,n,sudoku):
         for i in range(0,9):
@@ -44,11 +40,11 @@ def sudoku_solver(sudoku):
     """
     Solves a Sudoku puzzle and returns its unique solution.
 
-    Input
+    Parameters
         sudoku : 9x9 numpy array
             Empty cells are designated by 0.
 
-    Output
+    Returns
         9x9 numpy array of integers
             It contains the solution, if there is one. If there is no solution, all array entries should be -1.
     """        
@@ -63,8 +59,11 @@ def sudoku_solver(sudoku):
 
     return solved_sudoku
 
-
-print(sudokus[1])
-print(sudoku_solver(sudokus[1]))
-print(solutions[1])
-print()
+i = 0
+for sudoku in sudokus:
+    i++
+    print("This is sudoku number", i)
+    print(sudoku)
+    your_solution = sudoku_solver(sudokus)
+    print("Solution:")
+    print(your_solution)
